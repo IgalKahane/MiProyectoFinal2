dd18316a44d2454f30268de4c434dc1f95fd634d
IgalKahane <109997777+IgalKahane@users.noreply.github.com>
2022-08-22 16:17:24 -0300

    Ajustes Primera parte de proyecto final

---
+++ NombreAppi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAppi.ADO_.NET
+{
+    public static class NombreAppiHandler
+    {
+
+        public static bool GetNombreAppi()
+        {
+            bool nombreAppi = false;
+
+            
+            Console.WriteLine("      BIENVENIDO A TU COMPRA OTAKU!!     ");
+            
+
+            return nombreAppi = true;
+        }
+    }
+}
